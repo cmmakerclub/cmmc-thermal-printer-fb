19eb94704243826fe11f1904c03363986b3f0711
Nat Weerawan <nat.wrw@gmail.com>
2014-09-25 00:04:27 +0700

    autoload index.html

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+  		res.send(text);
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
